39febd36bc1b0224c985702390a5cff6b3710d26
orestis46 <orestis46@gmail.com>
2021-07-21 10:14:57 -0700

    Initial commit for 3 test files - Class 1, Excercise 1, step 5

---
+++ file2.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+interface, ip, mask = ('GigabitEthernet0/0', '10.0.0.1', '255.255.255.0')
+
+print('Interface: {}, IP address: {}, Subnet Mask: {}, format(interface, ip, mask)
+
