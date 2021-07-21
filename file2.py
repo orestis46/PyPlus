80cdcc558601d7a32355a9f6633e6fea23447e7d
orestis46 <orestis46@gmail.com>
2021-07-21 10:44:48 -0700

    Update file2.py , fixed missing closing parenthesis

--- file2.py
+++ file2.py
@@ -2,5 +2,6 @@
 
 interface, ip, mask = ('GigabitEthernet0/0', '10.0.0.1', '255.255.255.0')
 
-print('Interface: {}, IP address: {}, Subnet Mask: {}, format(interface, ip, mask)
+print('Interface: {}, IP address: {}, Subnet Mask: {}'.format(interface, ip, mask))
+
 
