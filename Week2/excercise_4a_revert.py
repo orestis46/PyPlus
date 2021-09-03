1e86f366e7b6c64a4d381688c1420a9ea9c6730e
orestis46 <33786387+orestis46@users.noreply.github.com>
2021-09-03 15:58:48 +0300

    Update excercise_4a_revert.py

--- Week2/excercise_4a_revert.py
+++ Week2/excercise_4a_revert.py
@@ -5,8 +5,8 @@ from netmiko import ConnectHandler
 
 device = {
     "host": "cisco3.lasthop.io",
-    "username": "pyclass",
-    "password": "fiction",
+    "username": null,
+    "password": null,
     "device_type": "cisco_ios",
 }
 
